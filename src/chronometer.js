dab288b7c3c6139112871e1023de4276576208a5
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-07-28 15:16:33 +0200

    all but bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,82 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+     
+    // +1 currenTime ++
+    // function printTimeCallback
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++ ; 
+        if(printTimeCallback) return printTimeCallback();
+
+    }  , 1000);
+    
+
+
+    
+
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
+
   }
 
   getSeconds() {
-    // ... your code goes here
+ 
+    return this.currentTime % 60;
+ 
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+   return value >=0 && value < 10 ? "0" + value : "" + value;
+
+    // this works too but I like ternary operators better
+    
+    //if(value >= 0 && value < 10){
+    //   return "0" + value;
+    // }
+
+    // else{
+    //   return "" + value;
+    // }
+
   }
 
   stop() {
-    // ... your code goes here
+
+      return clearInterval(this.intervalId);
+
   }
 
   reset() {
-    // ... your code goes here
+
+    this.currentTime = 0;
+
   }
 
   split() {
-    // ... your code goes here
+
+
+    //    return console.log(` ${this.computeTwoDigitNumber(this.getMinutes)}:${this.computeTwoDigitNumber(this.getSeconds)}`);
+
+    let val1 = this.computeTwoDigitNumber(this.getMinutes());
+    let val2 = this.computeTwoDigitNumber(this.getSeconds());
+    let result  = `${val1}:${val2}`;
+
+
+    return result; 
+    
+
   }
+
+
 }
+
